0e4460f68d689f937a26a9ad2830194501cc1c5b
k_95.21 <kohta0421kochi@gmail.com>
2023-10-25 23:23:43 +0900

    add new npm install swc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import styles from './page.module.css'
-import { Home } from "./components/Home";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Home />
     </main>
   )
 }
